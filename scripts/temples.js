30efa054a403b7950f28d72a02e39e3a9e9e0cd7
ruthbruce351 <ruthbruce351@gmail.com>
2024-10-05 09:07:12 -0500

    saving

---
+++ scripts/temples.js
@@ -0,0 +1,14 @@
+let currentDate = new Date();
+let currentyear = currentDate.getFullYear();
+
+let lastModified = new Date(document.lastModified);
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
